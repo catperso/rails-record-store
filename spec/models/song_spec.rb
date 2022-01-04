17d292e2aaebb8d44874dd71e0bf110ddac99e2b
Matt Casperson <mc.casperson@gmail.com>
2022-01-04 09:22:57 -0800

    add partial layouts to DRY up song and album views
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -4,4 +4,10 @@ describe Song do
   it { should belong_to(:album) }
   it { should validate_presence_of :name }
   it { should validate_length_of(:name).is_at_most(100) }
+
+  it("titleizes the name of a song") do
+    album = Album.create({name: "giant steps", genre: "jazz"})
+    song = Song.create({name: "do you like jazz", lyrics: "yes i do", album_id: album.id})
+    expect(song.name()).to(eq("Do You Like Jazz"))
+  end
 end
